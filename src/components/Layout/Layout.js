d82e72dcd069a52320a34af1cdf33907b9be7ebe
zoltan.pap <zoltan.pap@smartcsoport.hu>
2019-06-26 16:49:16 +0200

    builder container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactAux from '../../hoc/ReactAux';
+import classes from './Layout.css';
 
 const layout = ( props ) => (
   <ReactAux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-      <main>
+      <main className={classes.Content}>
         {props.children}
       </main>
   </ReactAux>
